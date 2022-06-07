a6a44d8863021599a607ba6a588e177fc84879ad
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2022-06-07 17:18:39 -0500

    iniciando projeto

---
+++ app/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+
+-- SET SESSION FOREIGN_KEY_CHECKS=0;
+
+-- create usuario (
+
+--     id int primary key auto_increment not null,
+--     nome varchar(100) not null,
+--     login varchar(100) not null,
+--     password varchar(10) not null,
+
+-- )
+
+-- create table tarefa (
+        
+--         id int not null primary key auto_increment,
+--         titulo varchar(100) not null,
+--         descricao text,
+--         status_tarefa int,
+--         usuario_id int not null reference usuario (id),
+--         prioridade int
+
+--         );
---
+++ app/src/test/java/com/lista_tarefa/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lista_tarefa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
