e86e5cfc5a1d594822466e33a3ce1933876e1651
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2022-06-08 16:10:47 -0500

    Salvando alterações

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -1,22 +1,21 @@
 
--- SET SESSION FOREIGN_KEY_CHECKS=0;
 
--- create usuario (
+create table usuario (
 
---     id int primary key auto_increment not null,
---     nome varchar(100) not null,
---     login varchar(100) not null,
---     password varchar(10) not null,
+    id int primary key auto_increment not null,
+    nome varchar(100) not null,
+    user varchar(100) not null,
+    senha varchar(10) not null,
 
--- )
+)
 
--- create table tarefa (
+create table tarefa (
         
---         id int not null primary key auto_increment,
---         titulo varchar(100) not null,
---         descricao text,
---         status_tarefa int,
---         usuario_id int not null reference usuario (id),
---         prioridade int
+        id int not null primary key auto_increment,
+        titulo varchar(100) not null,
+        descricao text,
+        status_tarefa int,
+        usuario_id int reference usuario (id),
+        prioridade int
 
---         );
+        );
